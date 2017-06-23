2d999a6adcb8d9a7013f07d52795e8f4a47359b8
Mathieu Chartier <mathieuc@google.com>
2017-06-23 12:59:56 -0700

    rubyx: Reduce system server verbosity [1/2]
    Test: lunch cannon-user && m
    Change-Id: I58d2e993ae9a7213e94661d22b47c28b0a2243e8
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Signed-off-by: postcumer <postcumer@tuta.io>

--- device.mk
+++ device.mk
@@ -337,6 +337,9 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
+# Reduce system server verbosity.
+PRODUCT_SYSTEM_SERVER_DEBUG_INFO := false
+
 # Always preopt extracted APKs to prevent extracting out of the APK for gms modules
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 
