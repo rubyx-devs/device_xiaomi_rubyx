5f2457c63d8a279c55a22464c2cea0d0c672b54a
postcumer <postcumer@tuta.io>
2025-01-20 20:42:53 +0530

    rubyx: Drop HotWordEnrollment * We are taking no-Google by default approach
    Signed-off-by: postcumer <postcumer@tuta.io>

--- device.mk
+++ device.mk
@@ -239,10 +239,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_nfc/android.hardware.nfc.uicc.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_nfc/android.hardware.nfc.xml
 
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml \
-    $(DEVICE_PATH)/configs/permissions/com.android.hotwordenrollment.common.util.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.android.hotwordenrollment.common.util.xml
-
 PRODUCT_PACKAGES += \
     android.hardware.hardware_keystore.km41.xml
 
