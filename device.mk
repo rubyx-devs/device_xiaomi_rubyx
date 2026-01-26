1be66f8d792dd7808b5349748226459879157a67
thisdialynx <130360106+thisdialynx@users.noreply.github.com>
2026-01-26 16:00:40 +0200

    Revert "rubyx: Disable UI blurs"
    This reverts commit f4cd32e5694cef2d96557a862318e7ef44b59ab8.
    
    An option is added for that in Accessibility > Colour and motion >
    Reduce blur effects
    
    Signed-off-by: thisdialynx <130360106+thisdialynx@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -79,9 +79,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
-# Blur
-TARGET_NOT_USES_BLUR := true
-
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
@@ -96,6 +93,7 @@ PRODUCT_PACKAGES += \
     android.hardware.ir-service.example
 
 # DeviceAsWebcam
+
 PRODUCT_PACKAGES += \
     DeviceAsWebcam
 
