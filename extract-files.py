0a0edcda187aab8531763bbe3b609af80e70292b
thisdialynx <130360106+thisdialynx@users.noreply.github.com>
2026-02-11 22:25:16 +0200

    rubyx: Do not depend on libui-v34

--- extract-files.py
+++ extract-files.py
@@ -109,7 +109,6 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/libeffecthal.base.so',
     'vendor/lib64/libmtkcam_grallocutils.so',
     'vendor/lib64/libmtkcam_3rdparty.vidhance.so'): blob_fixup()
-        .replace_needed('libui.so', 'libui-v34.so'),
 }
 
 module = ExtractUtilsModule(
